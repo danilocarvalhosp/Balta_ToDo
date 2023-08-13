15aaee3a27987934250a4fe5f96b60f0bf2f0631
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-13 10:47:06 -0300

    Configurando o EF

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using ToDo.Models;
+
+namespace ToDo.Data{
+    public class AppDbContext : DbContext{
+        public DbSet<TodoModel> ToDos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
